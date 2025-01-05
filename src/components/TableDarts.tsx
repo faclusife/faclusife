ae8b6381f9d39e35fb027bb14e5e6cc573092765
Bohdan <bohdan@itos.ai>
2025-01-05 11:29:12 -0500

    Upgraded next version and removed records label

--- src/components/TableDarts.tsx
+++ src/components/TableDarts.tsx
@@ -15,14 +15,6 @@ export default function TableDarts({ darts, isLoading }: Props) {
   }
   return (
     <div className="mt-6 px-4 sm:px-6 lg:px-8">
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">
-            Records
-          </h1>
-        </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none"></div>
-      </div>
       <div className="mt-8 flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
--- src/components/TableDartsAdmin.tsx
+++ src/components/TableDartsAdmin.tsx
@@ -200,13 +200,13 @@ export default function TableDartsAdmin({ darts }: Props) {
                     scope="col"
                     className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
                   >
-                    Name
+                    Score
                   </th>
                   <th
                     scope="col"
                     className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                   >
-                    Score
+                    Name
                   </th>
                   <th
                     scope="col"
