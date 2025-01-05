86d071cc06b48749c5e01547f01dfebe52f21370
Bohdan <bohdan@itos.ai>
2025-01-05 13:44:17 -0500

    Added head title to the page

--- src/pages/countdown/index.tsx
+++ src/pages/countdown/index.tsx
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import Countdown from "~/components/Countdown";
 
 export default function CountDownPage() {
-  return <Countdown />;
+  return (
+    <>
+      <Head>
+        <title>Countdown</title>
+      </Head>
+      <Countdown />
+    </>
+  );
 }
