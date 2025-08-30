1968420738d4d92f15143ce79f484979044eff7e
Bohdan <bohdan@itos.ai>
2025-08-29 22:12:05 -0400

    Removed weird ssh key

--- src/pages/countdown/index.tsx
+++ src/pages/countdown/index.tsx
@@ -5,6 +5,17 @@ export default function CountDownPage() {
   return (
     <>
       <Head>
+        <meta property="og:title" content="Countdown Timer" />
+        <meta
+          property="og:description"
+          content="Countdown to your special event in style!"
+        />
+        <meta
+          property="og:image"
+          content="https://example.com/path-to-image.jpg"
+        />
+        <meta property="og:url" content="https://example.com" />
+        <meta property="og:type" content="website" />
         <title>Countdown</title>
       </Head>
       <Countdown />
